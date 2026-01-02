c12b3f021eef615ef2d5f5eec41e2ff5004dbaa4
Christof Petig <christof.petig@arcor.de>
2026-01-02 20:09:57 +0100

    ptread cond stubs

--- thread-stub/pthread_cond_signal.c
+++ thread-stub/pthread_cond_signal.c
@@ -1,6 +1,10 @@
 #include "pthread_impl.h"
 
+__attribute__((import_module("$root"), import_name("[thread-yield]")))
+extern int thread_yield();
+
 int pthread_cond_signal(pthread_cond_t *c)
 {
+	thread_yield();
 	return 0;
 }
