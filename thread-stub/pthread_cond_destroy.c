e7fe931cd2fb63139a69849c392b71f3fa404ad4
Christof Petig <christof.petig@arcor.de>
2026-01-03 01:27:00 +0100

    proper waiting

--- thread-stub/pthread_cond_destroy.c
+++ thread-stub/pthread_cond_destroy.c
@@ -1,6 +1,11 @@
 #include "pthread_impl.h"
+#include <wasi/api.h>
 
 int pthread_cond_destroy(pthread_cond_t *c)
 {
+#ifdef __wasip3__
+	filesystem_stream_u8_drop_readable(c->__u.__i[0]);
+	filesystem_stream_u8_drop_writable(c->__u.__i[1]);
+#endif
 	return 0;
 }
